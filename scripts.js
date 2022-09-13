119e9a8ca5418085797542a6a8d4016d87cd0efe
Richard Sim <rdsim8589@gmail.com>
2022-09-13 11:37:46 -0400

    Add '/0' catch.

--- scripts.js
+++ scripts.js
@@ -1,17 +1,18 @@
 function add(a, b){
-    return a + b
+    return a + b;
 }
 
 function subtract(a, b) {
-    return a - b
+    return a - b;
 }
 
 function multiply(a, b) {
-    return a * b
+    return a * b;
 }
 
 function divide(a,b) {
-    return a/b
+    if (b == 0) return "nope";
+    return a/b;
 }
 
 
